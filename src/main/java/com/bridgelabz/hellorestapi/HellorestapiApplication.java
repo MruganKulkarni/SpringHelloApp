b4fa744a6b17c0e70f101d92515b0b06b0e025da
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-03-25 14:18:05 +0530

    [Mrugan Kulkarni]: Added Spring Initializr project

---
+++ src/main/java/com/bridgelabz/hellorestapi/HellorestapiApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellorestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/hellorestapi/HellorestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

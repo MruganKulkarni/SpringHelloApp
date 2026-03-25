1c20f777b5cf43005a1f99d8599d93a940154ce3
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-03-25 14:18:46 +0530

    [Mrugan Kulkarni]: UC1 Basic GET REST API

---
+++ src/main/java/com/bridgelabz/hellorestapi/controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellorestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz";
+    }
+}
